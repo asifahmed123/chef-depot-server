3375e115fcf53e1d5ee404b43840594813c459c0
asifahmed123 <ahmedasif092@gmail.com>
2023-05-02 02:24:49 +0600

    api added

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-
 const port = process.env.PORT || 3000
 
 app.use(cors())
+
+const dishes = require('./data/dishes.json')
+
+app.get('/', (req, res) => {
+     res.send('Hello World!')
+})
+app.get('/dishes', (req, res) => {
+     res.send(dishes)
+})
+app.get('/dishes/:id', (req, res) => {
+     const id = parseInt(req.params.id)
+     const selectedItem = dishes.find(d => d.id === id)
+     res.send(selectedItem)
+})
 
 app.listen(port, () => {
      console.log(`Example app listening on port ${port}`)
